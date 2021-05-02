01c23d9681d8e36e7a4b3bf98bd4842fa8e29ff1
Jacky Nguyen <nktpro@gmail.com>
2021-05-02 10:16:51 -0400

    Fix typo

--- toothpick-api/src/main/scala/dev/toothpick/proto/Mappers.scala
+++ toothpick-api/src/main/scala/dev/toothpick/proto/Mappers.scala
@@ -6,12 +6,12 @@ import com.google.protobuf.wrappers.BytesValue
 import scalapb.TypeMapper
 
 object Mappers {
-  implicit val bytesValueVersiontampMapper: TypeMapper[BytesValue, Versionstamp] =
+  implicit val bytesValueVersionstampMapper: TypeMapper[BytesValue, Versionstamp] =
     TypeMapper[BytesValue, Versionstamp](bytesValue => Versionstamp.fromBytes(bytesValue.value.toByteArray))(vs =>
       BytesValue.of(ByteString.copyFrom(vs.getBytes))
     )
 
-  implicit val byteStringVersiontampMapper: TypeMapper[ByteString, Versionstamp] =
+  implicit val byteStringVersionstampMapper: TypeMapper[ByteString, Versionstamp] =
     TypeMapper[ByteString, Versionstamp](bs => Versionstamp.fromBytes(bs.toByteArray))(vs =>
       ByteString.copyFrom(vs.getBytes)
     )
