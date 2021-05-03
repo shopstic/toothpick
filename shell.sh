cd8c9269bb63a7df1c103253d0b1650265769d5b
Jacky Nguyen <nktpro@gmail.com>
2021-05-03 00:48:11 -0400

    Fix shell user / group

--- shell.sh
+++ shell.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-docker build ./images/shell
-IMAGE_ID=$(docker build -q ./images/shell)
+CURRENT_UID=$(id -u)
+CURRENT_GID=$(id -g)
+
+BUILD_ARG=("--build-arg" "RUNNER_UID=${CURRENT_UID}" "--build-arg" "RUNNER_GID=${CURRENT_GID}")
+docker build "${BUILD_ARG[@]}" ./images/shell
+IMAGE_ID=$(docker build -q "${BUILD_ARG[@]}" ./images/shell | head -n1)
 
 docker run \
   -it \
@@ -10,8 +14,9 @@ docker run \
   --privileged \
   --hostname=toothpick-shell \
   -e GITHUB_TOKEN \
-  -v "${HOME}/Library/Caches/Coursier:/root/.cache/coursier" \
-  -v "${HOME}/Library/Caches/com.thesamet.scalapb.protocbridge.protocbridge:/root/.cache/protocbridge" \
+  -v "${HOME}/Library/Caches/Coursier:/home/runner/.cache/coursier" \
+  -v "${HOME}/.sbt:/home/runner/.sbt" \
+  -v "${HOME}/Library/Caches/com.thesamet.scalapb.protocbridge.protocbridge:/home/runner/.cache/protocbridge" \
   -v "${PWD}:${PWD}" \
   -w "${PWD}" \
   "${IMAGE_ID}" \
