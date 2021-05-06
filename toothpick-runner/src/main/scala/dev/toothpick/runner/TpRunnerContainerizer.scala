75d45757bc05af9247f6f370c47606ffce14da93
Jacky Nguyen <nktpro@gmail.com>
2021-05-06 18:34:10 -0400

    Support image authentication

--- toothpick-runner/src/main/scala/dev/toothpick/runner/TpRunnerContainerizer.scala
+++ toothpick-runner/src/main/scala/dev/toothpick/runner/TpRunnerContainerizer.scala
@@ -4,18 +4,46 @@ import better.files.File
 import com.google.cloud.tools.jib.api._
 import com.google.cloud.tools.jib.api.buildplan.{AbsoluteUnixPath, FileEntriesLayer}
 import com.google.cloud.tools.jib.event.events.TimerEvent
+import com.google.cloud.tools.jib.registry.credentials.DockerConfigCredentialRetriever
 import dev.chopsticks.fp.iz_logging.IzLogging
 import dev.toothpick.runner.intellij.TpIntellijTestRunArgsParser.TpRunnerContext
 import eu.timepit.refined.types.numeric.PosInt
 import eu.timepit.refined.types.string.NonEmptyString
-import eu.timepit.refined.auto._
 import logstage.Level
 import zio.{RIO, Task}
 
+import java.nio.file.Paths
+import java.util.function.Consumer
+
 object TpRunnerContainerizer {
+  final case class ContainerizerImageCredentials(
+    registry: NonEmptyString,
+    dockerConfigFile: NonEmptyString
+  )
+
+  final case class ContainerizerImageConfig(
+    name: NonEmptyString,
+    credentials: Option[ContainerizerImageCredentials] = None
+  ) {
+    def toImage(logger: Consumer[LogEvent]): RegistryImage = {
+      val ref = RegistryImage.named(name.value)
+      credentials match {
+        case Some(creds) =>
+          val retriever =
+            DockerConfigCredentialRetriever.create(creds.registry.value, Paths.get(creds.dockerConfigFile.value))
+
+          ref.addCredentialRetriever(() => {
+            retriever.retrieve(logger)
+          })
+        case None =>
+          ref
+      }
+    }
+  }
+
   final case class TpRunnerContainerizerConfig(
-    targetImage: NonEmptyString,
-    baseImage: NonEmptyString,
+    targetImage: ContainerizerImageConfig,
+    baseImage: ContainerizerImageConfig,
     runTimeoutSeconds: PosInt,
     killAfterRunTimeoutSeconds: PosInt,
     javaOptions: List[String]
@@ -78,12 +106,33 @@ object TpRunnerContainerizer {
           classpathListBuilder.result().mkString(":")
         ) :+ env.runnerClass
 
-        val targetImageString = containerizerConfig.targetImage.value
-        val baseImage = RegistryImage.named(
-          containerizerConfig.baseImage
-        )
-        val targetImage = RegistryImage
-          .named(targetImageString)
+        def handleEvent(event: JibEvent): Unit = {
+          event match {
+            case log: LogEvent if log.getLevel.compareTo(LogEvent.Level.PROGRESS) <= 0 =>
+              val level = log.getLevel match {
+                case LogEvent.Level.PROGRESS | LogEvent.Level.LIFECYCLE => Level.Info
+                case LogEvent.Level.ERROR => Level.Error
+                case LogEvent.Level.WARN => Level.Warn
+                case LogEvent.Level.INFO => Level.Info
+                case _ => Level.Debug
+              }
+
+              val message = log.getMessage
+
+              if (message.nonEmpty) {
+                logger.log(level)(s"${message -> "jib-message"}")
+              }
+
+            case timer: TimerEvent =>
+              logger.debug(
+                s"${timer.getState -> "jib-state"} ${timer.getDescription -> "" -> null}"
+              )
+            case _ =>
+          }
+        }
+
+        val baseImage = containerizerConfig.baseImage.toImage(handleEvent)
+        val targetImage = containerizerConfig.targetImage.toImage(handleEvent)
 
         val containerized = Jib
           .from(baseImage)
@@ -91,31 +140,9 @@ object TpRunnerContainerizer {
           .addFileEntriesLayer(classesLayerBuilder.build())
           .addFileEntriesLayer(testClassesLayerBuilder.build())
           .setEntrypoint(entrypoint.asJava)
-          .containerize(Containerizer.to(targetImage).addEventHandler { (event: JibEvent) =>
-            event match {
-              case log: LogEvent if log.getLevel.compareTo(LogEvent.Level.PROGRESS) <= 0 =>
-                val level = log.getLevel match {
-                  case LogEvent.Level.PROGRESS | LogEvent.Level.LIFECYCLE => Level.Info
-                  case LogEvent.Level.ERROR => Level.Error
-                  case LogEvent.Level.WARN => Level.Warn
-                  case _ => Level.Debug
-                }
+          .containerize(Containerizer.to(targetImage).addEventHandler(handleEvent))
 
-                val message = log.getMessage
-
-                if (message.nonEmpty) {
-                  logger.log(level)(s"${message -> "jib-message"}")
-                }
-
-              case timer: TimerEvent =>
-                logger.debug(
-                  s"${timer.getState -> "jib-state"} ${timer.getDescription -> "" -> null}"
-                )
-              case _ =>
-            }
-          })
-
-        s"$targetImageString@${containerized.getDigest.toString}"
+        s"${containerizerConfig.targetImage.name}@${containerized.getDigest.toString}"
       }
     }
   }
