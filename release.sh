ed1efdbb901e3e8651377ba7c7d443cb44228e96
Jacky Nguyen <nktpro@gmail.com>
2021-12-09 15:21:14 -0500

    Improve versioning

--- release.sh
+++ release.sh
@@ -1,6 +1,35 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+get_release_version() {
+  local VERSION_SBT
+  VERSION_SBT=$(cat ./version.sbt) || exit $?
+
+  local REGEX='ThisBuild / version := "([^"]+)"'
+
+  if [[ $VERSION_SBT =~ $REGEX ]]; then
+    local CURRENT_VERSION="${BASH_REMATCH[1]}"
+
+    if [[ $CURRENT_VERSION =~ -SNAPSHOT$ ]]; then
+      CURRENT_VERSION=${CURRENT_VERSION/-SNAPSHOT/}
+      local GIT_SHA
+      local GIT_COMMIT_COUNT
+      local SHORTENED_COMMIT_SHA
+
+      GIT_SHA=$(git rev-parse HEAD) || exit $?
+      SHORTENED_COMMIT_SHA=$(echo "${GIT_SHA}" | cut -c 1-7) || exit $?
+      GIT_COMMIT_COUNT=$(git rev-list --count HEAD) || exit $?
+
+      echo "${CURRENT_VERSION}+${GIT_COMMIT_COUNT}-${SHORTENED_COMMIT_SHA}"
+    else
+      echo "${CURRENT_VERSION}"
+    fi
+  else
+    >&2 echo "Cannot determine version from version.sbt"
+    exit 1
+  fi
+}
+
 push_helm_chart() {
   export HELM_CHART_VERSION=${1:?"Helm chart version is required"}
   export HELM_APP_VERSION=${2:?"Helm chart app version is required"}
