6516097c7eda635506fb4d2816ebefcb2179bd08
Jacky Nguyen <nktpro@gmail.com>
2021-12-10 03:25:24 -0500

    Fix image hash

--- nix/server-image.nix
+++ nix/server-image.nix
@@ -11,7 +11,10 @@ let
   baseImage = dockerTools.pullImage {
     imageName = "docker.io/library/ubuntu";
     imageDigest = "sha256:93a94c12448f393522f44d8a1b34936b7f76890adea34b80b87a245524d1d574";
-    sha256 = "sha256-rAy5pQ3wrBSNzTqwX8WTEfTqVmILPGqQg/j69v2C4EM=";
+    sha256 =
+      if stdenv.isx86_64 then
+        "sha256-s+eiBV7iA29UxnWg9rI2oMpwH/CNc9QRPbOUJJyBoTk=" else
+        "sha256-rAy5pQ3wrBSNzTqwX8WTEfTqVmILPGqQg/j69v2C4EM=";
   };
   javaSecurityOverrides = writeTextFile {
     name = "java.security.overrides";
